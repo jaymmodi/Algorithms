cc7553f535f57d8baa8e0f83659ac24a11c660ee
Jay Modi <jmodi@fhlbdm.com>
2017-09-14 09:09:17 -0500

    commit -2

--- src/Strings/CurrentString.java
+++ src/Strings/CurrentString.java
@@ -4,12 +4,20 @@ public class CurrentString {
 
     public String str;
     public int lastRowIndex;
-    public   int lastColumnIndex;
+    public int lastColumnIndex;
+    public boolean[][] visited;
 
-    public CurrentString(String str, int lastRowIndex, int lastColumnIndex) {
+    public CurrentString(String str, int lastRowIndex, int lastColumnIndex, boolean[][] visited) {
         this.str = str;
         this.lastRowIndex = lastRowIndex;
         this.lastColumnIndex = lastColumnIndex;
+
+        this.visited = new boolean[visited.length][visited[0].length];
+        for (int i = 0; i < visited.length; i++) {
+            for (int j = 0; j < visited[0].length; j++) {
+                this.visited[i][j] = visited[i][j];
+            }
+        }
     }
 
 }
--- src/Strings/WordSearch.java
+++ src/Strings/WordSearch.java
@@ -1,6 +1,8 @@
 package Strings;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import static java.util.Arrays.asList;
 
@@ -15,103 +17,109 @@ public class WordSearch {
 //        };
 
         char[][] board = {
-                {'a', 'b'},
-                {'a', 'a'}
+                {'a', 'b', 'c'},
+                {'a', 'e', 'd'},
+                {'a', 'f', 'g'}
         };
 
-        List<String> words = asList("aba", "baa", "bab", "aaab", "aaa", "aaaa", "aaba");
-        List<String> words1 = findWords(board, words);
+        List<String> words = asList("eaabcdgfa");
+        WordSearch wordSearch = new WordSearch();
+        List<String> words1 = wordSearch.findWords(board, words);
 
         for (String s : words1) {
             System.out.println(s);
         }
     }
 
-    private static List<String> findWords(char[][] board, List<String> words) {
+    private List<String> findWords(char[][] board, List<String> words) {
         int rowSize = board.length;
         int columnSize = board[0].length;
 
-        Set<String> uniqueWords = new HashSet<>(words);
-
         List<String> result = new ArrayList<>();
 
-        for (String word : uniqueWords) {
-
-            LinkedList<CurrentString> startStrings = new LinkedList<>();
-            boolean innerBreak = false;
-            for (int rowIndex = 0; rowIndex < rowSize; rowIndex++) {
-                for (int columnIndex = 0; columnIndex < columnSize; columnIndex++) {
-                    boolean[][] visited = new boolean[rowSize][columnSize];
-
-                    String boardCharString = String.valueOf(board[rowIndex][columnIndex]);
-                    visited[rowIndex][columnIndex] = true;
-                    if (word.contains(boardCharString)) {
-                        startStrings.add(new CurrentString(boardCharString, rowIndex, columnIndex));
-                        if (goFindString(startStrings, board, word, rowSize, columnSize, visited)) {
-                            result.add(word);
-                            innerBreak = true;
-                            break;
-                        }
-                    }
-
-                }
-                if (innerBreak) {
-                    break;
-                }
+        for (String word : words) {
+            String found = findWord(board, rowSize, columnSize, word);
+            if (null != found && !result.contains(found)) {
+                result.add(found);
             }
-
         }
 
         return result;
     }
 
-    private static boolean goFindString(LinkedList<CurrentString> startStrings, char[][] board, String word, int rowSize, int columnSize, boolean[][] visited) {
+    private String findWord(char[][] board, int rowSize, int columnSize, String word) {
+        LinkedList<CurrentString> queue = new LinkedList<>();
+
+        for (int rowIndex = 0; rowIndex < rowSize; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < columnSize; columnIndex++) {
+                boolean[][] visited = new boolean[rowSize][columnSize];
+
+                String boardCharString = String.valueOf(board[rowIndex][columnIndex]);
+
+                if (word.startsWith(boardCharString)) {
+                    visited[rowIndex][columnIndex] = true;
+                    queue.add(new CurrentString(boardCharString, rowIndex, columnIndex, visited));
+                    if (bfs(queue, board, word, rowSize, columnSize)) {
+                        return word;
+                    }
+                }
+
+            }
+        }
+        return null;
+    }
+
+    private boolean bfs(LinkedList<CurrentString> queue, char[][] board, String word, int rowSize, int columnSize) {
 
         boolean isMatch = false;
-        while (!startStrings.isEmpty()) {
-            CurrentString currentString = startStrings.poll();
+        while (!queue.isEmpty()) {
+            CurrentString currentString = queue.poll();
             if (currentString.str.equals(word)) {
                 isMatch = true;
                 break;
             }
 
-            if (currentString.lastColumnIndex < columnSize - 1 && !visited[currentString.lastRowIndex][currentString.lastColumnIndex + 1]) { // right neighbor
+            if (currentString.lastColumnIndex < columnSize - 1 && !currentString.visited[currentString.lastRowIndex][currentString.lastColumnIndex + 1]) { // right neighbor
                 String rightNeighbor = String.valueOf(getNextHorizontalRight(board, currentString.lastRowIndex, currentString.lastColumnIndex));
                 String updatedString = currentString.str + rightNeighbor;
 
-                if (word.contains(updatedString)) {
-                    visited[currentString.lastRowIndex][currentString.lastColumnIndex + 1] = true;
-                    startStrings.add(new CurrentString(updatedString, currentString.lastRowIndex, currentString.lastColumnIndex + 1));
+                if (word.startsWith(updatedString)) {
+                    boolean[][] updatedVisited = copyArray(currentString.visited);
+                    updatedVisited[currentString.lastRowIndex][currentString.lastColumnIndex + 1] = true;
+                    queue.add(new CurrentString(updatedString, currentString.lastRowIndex, currentString.lastColumnIndex + 1, updatedVisited));
                 }
             }
 
-            if (currentString.lastColumnIndex > 0 && !visited[currentString.lastRowIndex][currentString.lastColumnIndex - 1]) { // left neighbor
+            if (currentString.lastColumnIndex > 0 && !currentString.visited[currentString.lastRowIndex][currentString.lastColumnIndex - 1]) { // left neighbor
                 String leftNeighbor = String.valueOf(getNextHorizontalLeft(board, currentString.lastRowIndex, currentString.lastColumnIndex));
                 String updatedString = currentString.str + leftNeighbor;
 
-                if (word.contains(updatedString)) {
-                    visited[currentString.lastRowIndex][currentString.lastColumnIndex - 1] = true;
-                    startStrings.add(new CurrentString(updatedString, currentString.lastRowIndex, currentString.lastColumnIndex - 1));
+                if (word.startsWith(updatedString)) {
+                    boolean[][] updatedVisited = copyArray(currentString.visited);
+                    updatedVisited[currentString.lastRowIndex][currentString.lastColumnIndex - 1] = true;
+                    queue.add(new CurrentString(updatedString, currentString.lastRowIndex, currentString.lastColumnIndex - 1, updatedVisited));
                 }
             }
 
-            if (currentString.lastRowIndex < rowSize - 1 && !visited[currentString.lastRowIndex + 1][currentString.lastColumnIndex]) { // down neighbor
+            if (currentString.lastRowIndex < rowSize - 1 && !currentString.visited[currentString.lastRowIndex + 1][currentString.lastColumnIndex]) { // down neighbor
                 String downNeighbor = String.valueOf(getNextVerticalDown(board, currentString.lastRowIndex, currentString.lastColumnIndex));
                 String updatedString = currentString.str + downNeighbor;
 
-                if (word.contains(updatedString)) {
-                    visited[currentString.lastRowIndex + 1][currentString.lastColumnIndex] = true;
-                    startStrings.add(new CurrentString(updatedString, currentString.lastRowIndex + 1, currentString.lastColumnIndex));
+                if (word.startsWith(updatedString)) {
+                    boolean[][] updatedVisited = copyArray(currentString.visited);
+                    updatedVisited[currentString.lastRowIndex + 1][currentString.lastColumnIndex] = true;
+                    queue.add(new CurrentString(updatedString, currentString.lastRowIndex + 1, currentString.lastColumnIndex, updatedVisited));
                 }
             }
 
-            if (currentString.lastRowIndex > 0 && !visited[currentString.lastRowIndex - 1][currentString.lastColumnIndex]) { // up neighbor
+            if (currentString.lastRowIndex > 0 && !currentString.visited[currentString.lastRowIndex - 1][currentString.lastColumnIndex]) { // up neighbor
                 String upNeighbor = String.valueOf(getNextVerticalUp(board, currentString.lastRowIndex, currentString.lastColumnIndex));
                 String updatedString = currentString.str + upNeighbor;
 
-                if (word.contains(updatedString)) {
-                    visited[currentString.lastRowIndex - 1][currentString.lastColumnIndex] = true;
-                    startStrings.add(new CurrentString(updatedString, currentString.lastRowIndex - 1, currentString.lastColumnIndex));
+                if (word.startsWith(updatedString)) {
+                    boolean[][] updatedVisited = copyArray(currentString.visited);
+                    updatedVisited[currentString.lastRowIndex - 1][currentString.lastColumnIndex] = true;
+                    queue.add(new CurrentString(updatedString, currentString.lastRowIndex - 1, currentString.lastColumnIndex, updatedVisited));
                 }
             }
         }
@@ -119,20 +127,30 @@ public class WordSearch {
         return isMatch;
     }
 
-    private static char getNextHorizontalRight(char[][] board, int rowIndex, int columnIndex) {
+    private char getNextHorizontalRight(char[][] board, int rowIndex, int columnIndex) {
         return board[rowIndex][columnIndex + 1];
     }
 
-    private static char getNextHorizontalLeft(char[][] board, int rowIndex, int columnIndex) {
+    private char getNextHorizontalLeft(char[][] board, int rowIndex, int columnIndex) {
         return board[rowIndex][columnIndex - 1];
     }
 
-    private static char getNextVerticalUp(char[][] board, int rowIndex, int columnIndex) {
+    private char getNextVerticalUp(char[][] board, int rowIndex, int columnIndex) {
         return board[rowIndex - 1][columnIndex];
     }
 
-    private static char getNextVerticalDown(char[][] board, int rowIndex, int columnIndex) {
+    private char getNextVerticalDown(char[][] board, int rowIndex, int columnIndex) {
         return board[rowIndex + 1][columnIndex];
+    }
+
+    private boolean[][] copyArray(boolean[][] input) {
+        boolean[][] newArray = new boolean[input.length][];
+
+        for (int i = 0; i < input.length; i++) {
+            newArray[i] = input[i].clone();
+        }
+
+        return newArray;
     }
 
 }
