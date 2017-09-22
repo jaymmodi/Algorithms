05aad7eab22c1df26a39758fd9ebc17a50862410
Jay Modi <modi.jay6@gmail.com>
2017-09-21 19:29:36 -0500

    one liner for finding missing number - magic of streams

---
+++ src/bits/XOR.java
@@ -0,0 +1,17 @@
+package bits;
+
+import java.util.Arrays;
+
+public class XOR {
+
+    public static void main(String[] args) {
+        int[] array = {5, 5, 2, 2, 8};
+
+        System.out.println(singleNumber(array));
+    }
+
+    private static int singleNumber(int[] array) {
+        return Arrays.stream(array).reduce(0, (x, y) -> x ^ y);
+    }
+
+}
