464c789e09b1f44930f0fd18c1df96f7b61d15b8
Jay Modi <modi.jay6@gmail.com>
2017-09-23 16:30:01 -0500

    permutations of string

---
+++ src/Recursion/Backtracking/Permutation.java
@@ -0,0 +1,39 @@
+package Recursion.Backtracking;
+
+public class Permutation {
+
+    public static void main(String[] args) {
+        String str = "ab";
+
+        permute(str);
+    }
+
+    private static void permute(String str) {
+        char[] chars = str.toCharArray();
+        boolean[] visited = new boolean[chars.length];
+
+        for (int i = 0; i < chars.length; i++) {
+            visited[i] = true;
+            permute(chars, visited, "" + chars[i]);
+            visited[i] = false;
+        }
+    }
+
+    private static void permute(char[] chars, boolean[] visited, String currentString) {
+        if (currentString.length() == chars.length) {
+            System.out.println(currentString);
+        } else {
+            for (int i = 0; i < chars.length; i++) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    currentString = currentString + String.valueOf(chars[i]);
+
+                    permute(chars, visited, currentString);
+
+                    visited[i] = false;
+                    currentString = currentString.substring(0, currentString.length() - 1);
+                }
+            }
+        }
+    }
+}
