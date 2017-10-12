b6a23110ab4353645c98d4e073ccf6aceb6a6d04
Jay Modi <modi.jay6@gmail.com>
2017-10-12 19:55:42 +0530

    two sum trees - leetcode

---
+++ src/Trees/TwoSumBST.java
@@ -0,0 +1,57 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoSumBST {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        TreeNode left = new TreeNode(3);
+        TreeNode right = new TreeNode(6);
+
+        left.setLeft(new TreeNode(2));
+        left.setRight(new TreeNode(4));
+
+        right.setRight(new TreeNode(7));
+
+        root.setRight(right);
+        root.setLeft(left);
+
+        System.out.println(findTarget(root, 9));
+    }
+
+    public static boolean findTarget(TreeNode root, int k) {
+        if (root == null) {
+            return false;
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+        inOrder(root, numbers);
+
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        while (left < right) {
+            if (numbers.get(left) + numbers.get(right) == k) {
+                return true;
+            } else if (numbers.get(left) + numbers.get(right) > k) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        return false;
+    }
+
+    private static void inOrder(TreeNode root, List<Integer> numbers) {
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.getLeft(), numbers);
+        numbers.add(root.getVal());
+        inOrder(root.getRight(), numbers);
+    }
+}
