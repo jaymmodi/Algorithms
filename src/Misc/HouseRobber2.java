7df71bdf24f061b16d54ca7c5d7a46cee355802e
Jay Modi <modi.jay6@gmail.com>
2017-10-13 17:44:12 +0530

    house robber 2 - leetcode solution

--- src/Misc/HouseRobber2.java
+++ src/Misc/HouseRobber2.java
@@ -1,26 +1,33 @@
 package Misc;
 
-public class HouseRobber1 {
+public class HouseRobber2 {
 
     public static void main(String[] args) {
-//        int[] nums = {2, 3, 2};
-//        int[] nums = {100, 555, 28, 6, 1000};
         int[] nums = {4, 1, 2, 7, 5, 3, 1};
-        System.out.println(rob(nums, 0, nums.length - 1));
 
-        System.out.println(Math.max(rob(nums, 1, nums.length - 1), rob(nums, 0, nums.length - 2)));
+        System.out.println(rob(nums));
     }
 
-    public static int rob(int[] nums, int start, int end) {
+    public static int rob(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
+
         if (nums.length == 1) {
-            return nums[start];
+            return nums[0];
         }
+
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+
+        return Math.max(rob(nums, 1, nums.length - 1), rob(nums, 0, nums.length - 2));
+    }
+
+    public static int rob(int[] nums, int start, int end) {
         int max = 0;
         int previousMax = 0;
-        if (nums.length >= 2) {
+        if (nums.length > 2) {
             max = Math.max(nums[end], nums[end - 1]);
             previousMax = nums[end];
         }
