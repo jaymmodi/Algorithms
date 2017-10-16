f9674db5533e10d1175c3e1a475f09169f659429
Jay Modi <modi.jay6@gmail.com>
2017-10-16 17:03:19 +0530

    top k frequent numbers - leetcode - priority queue solution - but slow

---
+++ src/Heap/Heap.java
@@ -0,0 +1,36 @@
+package Heap;
+
+import java.util.*;
+
+public class Heap {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+
+        topKFrequent(nums, 2).forEach(System.out::println);
+    }
+
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+        if (nums.length == 0 || nums == null) {
+            return new ArrayList<>();
+        }
+
+        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> countMap = new HashMap<>();
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a, b) -> (b.getValue() - a.getValue()));
+
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> set : countMap.entrySet()) {
+            queue.offer(set);
+        }
+
+        while (result.size() < k) {
+            result.add(queue.poll().getKey());
+        }
+
+        return result;
+    }
+}
