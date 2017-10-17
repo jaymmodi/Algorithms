8f527407dcf235a4a11a2930f5f5c6a99d7ab04b
Jay Modi <modi.jay6@gmail.com>
2017-10-17 16:42:12 +0530

    with streams - slow solution

--- src/Misc/FizzBizz.java
+++ src/Misc/FizzBizz.java
@@ -2,11 +2,14 @@ package Misc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FizzBizz {
 
     public static void main(String[] args) {
-        fizzBuzz(15).forEach(System.out::println);
+//        fizzBuzz(15).forEach(System.out::println);
+        withStream(15).forEach(System.out::println);
     }
 
     public static List<String> fizzBuzz(int n) {
@@ -28,5 +31,23 @@ public class FizzBizz {
         }
 
         return result;
+    }
+
+    public static List<String> withStream(int n) {
+
+        return IntStream.rangeClosed(1, n)
+                .parallel()
+                .mapToObj((int i) -> {
+                    if (i % 3 == 0 && i % 5 == 0) {
+                        return "FizzBuzz";
+                    } else if (i % 3 == 0) {
+                        return "Fizz";
+                    } else if (i % 5 == 0) {
+                        return "Buzz";
+                    } else {
+                        return "" + i;
+                    }
+                })
+                .collect(Collectors.toList());
     }
 }
