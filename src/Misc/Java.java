9dcbaeed6ae06ecf88a11d01f87a3c134a4f4533
Jay Modi <modi.jay6@gmail.com>
2017-11-03 16:59:02 -0500

    playing around with negative and positive binary numbers

---
+++ src/Misc/Java.java
@@ -0,0 +1,16 @@
+package Misc;
+
+public class Java {
+    public static void main(String[] args) {
+//        System.out.println(Math.pow(2, 13));
+//        System.out.println("Aa".hashCode());
+//        System.out.println("BB".hashCode());
+
+        System.out.println(4 >>> 1);
+        System.out.println(Integer.toBinaryString(-1));
+        int num = 1 << 31;
+        System.out.println(num);
+        System.out.println(Integer.toBinaryString(num - 1));
+        System.out.println(Integer.toBinaryString(-2));
+    }
+}
