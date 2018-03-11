f0842c914697cc34dc93172aa8be555bfe2e97aa
Jay Modi <modi.jay6@gmail.com>
2018-03-11 16:37:07 -0500

    contains duplicate 1 -- leetcode

---
+++ src/Arrays/ContainDuplicates1.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainDuplicates1 {
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1, 2, 3, 2}));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            } else {
+                set.add(num);
+            }
+        }
+
+        return false;
+    }
+}
