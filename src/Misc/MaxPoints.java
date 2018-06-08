5a3cc38424a18e7153aedced631beac243381808
Jay Modi <modi.jay6@gmail.com>
2018-06-08 01:05:30 -0500

    max points commit 2

--- src/Misc/MaxPoints.java
+++ src/Misc/MaxPoints.java
@@ -9,23 +9,23 @@ public class MaxPoints {
 
     public static void main(String[] args) {
 
-//        Point[] points1 = {
-//                new Point(1, 1),
-//                new Point(3, 2),
-//                new Point(5, 3),
-//                new Point(4, 1),
-//                new Point(2, 3),
-//                new Point(1, 4)
-//        };
+        Point[] points1 = {
+                new Point(1, 1),
+                new Point(3, 2),
+                new Point(5, 3),
+                new Point(4, 1),
+                new Point(2, 3),
+                new Point(1, 4)
+        };
 
-//        Point[] points2 = {
-//                new Point(1, 1),
-//                new Point(2, 2),
-//                new Point(3, 3),
-//                new Point(4, 4),
-//                new Point(2, 3),
-//                new Point(3, 4)
-//        };
+        Point[] points2 = {
+                new Point(1, 1),
+                new Point(2, 2),
+                new Point(3, 3),
+                new Point(4, 4),
+                new Point(2, 3),
+                new Point(3, 4)
+        };
 
         Point[] points3 = {
                 new Point(0, 0),
@@ -34,11 +34,12 @@ public class MaxPoints {
         };
 
 
-//        Point[] points4 = {
-//                new Point(4, 0),
-//                new Point(4, -1),
-//                new Point(4, 5)
-//        };
+        Point[] points4 = {
+                new Point(4, 0),
+                new Point(4, -1),
+                new Point(4, 5),
+                new Point(4, 5)
+        };
 
         Point[] points5 = {
                 new Point(1, 1),
@@ -52,13 +53,20 @@ public class MaxPoints {
                 new Point(0, 0)
         };
 
+        Point[] points7 = {
+                new Point(1, 1),
+                new Point(0, 0),
+                new Point(0, 0)
+        };
 
-//        System.out.println(maxPoints(points2));
-//        System.out.println(maxPoints(points1));
+
+        System.out.println(maxPoints(points1));
+        System.out.println(maxPoints(points2));
         System.out.println(maxPoints(points3));
-//        System.out.println(maxPoints(points4));
+        System.out.println(maxPoints(points4));
         System.out.println(maxPoints(points5));
         System.out.println(maxPoints(points6));
+        System.out.println(maxPoints(points7));
 
     }
 
@@ -69,26 +77,48 @@ public class MaxPoints {
         }
 
         Map<Double, List<List<Point>>> slopeMap = new HashMap<>();
+        Map<Integer, Map<Integer, Integer>> pointsCount = new HashMap<>();
+        List<Point> maxLine = null;
         int max = Integer.MIN_VALUE;
+
+        for (Point point : points) {
+            if (pointsCount.containsKey(point.x)) {
+                Map<Integer, Integer> yCordinateMap = pointsCount.get(point.x);
+
+                if (yCordinateMap.containsKey(point.y)) {
+                    Integer yCount = yCordinateMap.get(point.y);
+
+                    yCordinateMap.put(point.y, ++yCount);
+                } else {
+                    yCordinateMap.put(point.y, 1);
+                }
+
+            } else {
+                Map<Integer, Integer> yCordinateMap = new HashMap<>();
+                yCordinateMap.put(point.y, 1);
+
+                pointsCount.put(point.x, yCordinateMap);
+            }
+        }
 
         for (int i = 0; i < points.length; i++) {
             for (int j = i + 1; j < points.length; j++) {
                 Point point1 = points[i];
                 Point point2 = points[j];
 
-
                 double slope = slope(point1, point2);
-                if (slopeMap.containsKey(slope)) {
+                if (slopeMap.containsKey(slope) && !isEqual(point1, point2)) {
                     boolean newLineFlag = true;
                     List<List<Point>> lines = slopeMap.get(slope);
 
                     for (List<Point> line : lines) {
-                        if ((slope(line.get(0), point2) == slope || isEqual(point2, line.get(0))) && !line.contains(point2)) {
+                        if (slope(line.get(0), point2) == slope && !line.contains(point2) && !hasPoint(line,point2)) {
                             line.add(point2);
                             newLineFlag = false;
 
                             if (line.size() > max) {
                                 max = line.size();
+                                maxLine = line;
                             }
                         }
                     }
@@ -101,7 +131,7 @@ public class MaxPoints {
                         lines.add(newLine);
                     }
 
-                } else {
+                } else if (!isEqual(point1, point2)) {
 
                     List<Point> newLine = new ArrayList<>();
                     newLine.add(point1);
@@ -113,13 +143,24 @@ public class MaxPoints {
 
                     if (max == Integer.MIN_VALUE) {
                         max = 2;
+                        maxLine = newLine;
                     }
                 }
 
             }
         }
 
+        if (maxLine != null) {
+            for (Point point : maxLine) {
+                int count = pointsCount.get(point.x).get(point.y);
 
+                if (count > 1) {
+                    max = max + count - 1;
+                }
+            }
+        } else {
+            max = pointsCount.get(points[0].x).get(points[0].y);
+        }
         return max;
     }
 
@@ -135,5 +176,14 @@ public class MaxPoints {
         }
 
         return slope == -0.0 ? 0.0 : slope;
+    }
+
+    private static boolean hasPoint(List<Point> points, Point point) {
+        for (Point point1 : points) {
+            if (isEqual(point1, point)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
