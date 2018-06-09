f7e10a10015c471c64174c0066cded051b30bd0e
Jay Modi <modi.jay6@gmail.com>
2018-06-08 23:30:45 -0500

    max number without recursive

--- src/Trees/MaxNumber.java
+++ src/Trees/MaxNumber.java
@@ -1,11 +1,15 @@
 package Trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MaxNumber {
 
     public static void main(String[] args) {
         TreeNode root = SampleTree.getBinaryTree();
 
         System.out.println(maxNodeValue(root));
+        System.out.println(maxNodeValueNonRecursive(root));
     }
 
     private static int maxNodeValue(TreeNode root) {
@@ -20,5 +24,32 @@ public class MaxNumber {
         int max = Math.max(rootValue, leftValue);
 
         return Math.max(max, rightValue);
+    }
+
+    private static int maxNodeValueNonRecursive(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int max = Integer.MIN_VALUE;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+
+            if (node.val > max) {
+                max = node.val;
+            }
+
+            if (null != node.left) {
+                queue.add(node.left);
+            }
+
+            if (null != node.right) {
+                queue.add(node.right);
+            }
+        }
+        return max;
     }
 }
--- src/Trees/SampleTree.java
+++ src/Trees/SampleTree.java
@@ -3,13 +3,12 @@ package Trees;
 public class SampleTree {
 
     public static TreeNode getBinaryTree() {
-        TreeNode root = new TreeNode(100);
+        TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
 
         root.left.left = new TreeNode(4);
         root.left.right = new TreeNode(5);
-        root.left.right.right = new TreeNode(6);
 
         return root;
     }
