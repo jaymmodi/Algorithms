b786c5598ec9797edf7f61f33f19a08d9e9ea5ad
Jay Modi <modi.jay6@gmail.com>
2018-06-14 23:21:32 -0500

    unique morse code records

---
+++ src/Strings/UniqueMorseCodes.java
@@ -0,0 +1,33 @@
+package Strings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueMorseCodes {
+    public static final String[] codes = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+    public static void main(String[] args) {
+        String[] words = {"gin", "zen", "gig", "msg"};
+//        String[] words = {"abc"};
+
+        System.out.println(uniqueMorseRepresentations(words));
+    }
+
+    public static int uniqueMorseRepresentations(String[] words) {
+        Set<String> result = new HashSet<>();
+
+        for (String word : words) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Character c : word.toCharArray()) {
+                int index = c - 97;
+                stringBuilder.append(codes[index]);
+            }
+            result.add(stringBuilder.toString());
+        }
+
+
+        return result.size();
+    }
+
+
+}
