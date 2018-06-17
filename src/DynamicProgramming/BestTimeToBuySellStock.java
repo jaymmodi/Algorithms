e3daa33c209d584d2f79d187d7c54e770c16a720
Jay Modi <modi.jay6@gmail.com>
2018-06-16 21:13:17 -0500

    stock sell buy II, leetcode, time limit exceeded solution

---
+++ src/DynamicProgramming/BestTimeToBuySellStock.java
@@ -0,0 +1,42 @@
+package DynamicProgramming;
+
+public class BestTimeToBuySellStock {
+    public static void main(String[] args) {
+        int prices1[] = {7, 1, 5, 3, 6, 4};
+        int prices2[] = {3, 1};
+        int prices3[] = {1, 2, 3, 4, 5};
+        int prices4[] = {2, 6, 5, 0, 3};
+        int prices5[] = {1, 7, 4, 2};
+
+        System.out.println(maxProfit(prices1));
+        System.out.println(maxProfit(prices2));
+        System.out.println(maxProfit(prices3));
+        System.out.println(maxProfit(prices4));
+        System.out.println(maxProfit(prices5));
+    }
+
+    public static int maxProfit(int[] prices) {
+        return maxProfitHelper(prices, 0);
+    }
+
+    private static int maxProfitHelper(int[] prices, int start) {
+        if (start >= prices.length - 1) {
+            return 0;
+        }
+
+        if (start == prices.length - 2) {
+            return prices[start + 1] > prices[start] ? prices[start + 1] - prices[start] : 0;
+        }
+
+        int profit = 0;
+        for (int buyingIndex = start; buyingIndex < (prices.length); buyingIndex++) {
+            for (int sellingIndex = buyingIndex + 1; sellingIndex < prices.length; sellingIndex++) {
+                int currentProfit = prices[sellingIndex] - prices[buyingIndex];
+
+                profit = Math.max(profit, currentProfit + maxProfitHelper(prices, sellingIndex + 1));
+            }
+        }
+
+        return profit < 0 ? 0 : profit;
+    }
+}
