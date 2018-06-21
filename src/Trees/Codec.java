6d9d98161fedb2eff14e7ae0bd102f9102ad20cd
Jay Modi <modi.jay6@gmail.com>
2018-06-21 00:46:17 -0500

    serialize and deserialize with BFS

--- src/Trees/Codec.java
+++ src/Trees/Codec.java
@@ -1,26 +1,72 @@
 package Trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Codec {
     public static void main(String[] args) {
-        TreeNode node = SampleTree.getBST();
+        TreeNode node = SampleTree.getBinaryTree2();
 
         Codec codec = new Codec();
         String serialize = codec.serialize(node);
+        System.out.println("serialize = " + serialize);
 
-        codec.deserialize(serialize);
+        TreeNode deserialize = codec.deserialize(serialize);
+        System.out.println(deserialize);
     }
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if (null == root) {
-            return "";
+        if (root == null) {
+            return "#";
         }
 
-        return serialize(root.left) + root.val + serialize(root.right);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (null == node) {
+                sb.append("#");
+            } else {
+                sb.append(node.val);
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+            sb.append(",");
+        }
+
+        return sb.toString();
     }
+
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        return null;
+        if (data.equals("#")) {
+            return null;
+        }
+
+        String[] values = data.split(",");
+        Queue<TreeNode> nodes = new LinkedList<>();
+        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+
+        nodes.add(root);
+
+        for (int i = 1; i < values.length; i++) {
+            TreeNode currentNode = nodes.poll();
+
+            if (!values[i].equals("#")) {
+                currentNode.left = new TreeNode(Integer.parseInt(values[i]));
+                nodes.add(currentNode.left);
+            }
+            if (!values[++i].equals("#")) {
+                currentNode.right = new TreeNode(Integer.parseInt(values[i]));
+                nodes.add(currentNode.right);
+            }
+        }
+
+
+        return root;
     }
 }
--- src/Trees/SampleTree.java
+++ src/Trees/SampleTree.java
@@ -104,11 +104,15 @@ public class SampleTree {
     }
 
     public static TreeNode getBinaryTree2() {
-        TreeNode root = new TreeNode(1);
+        TreeNode root = new TreeNode(5);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
 
-        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(2);
+        root.right.right = new TreeNode(4);
+
+        root.right.left.left = new TreeNode(3);
+        root.right.left.right = new TreeNode(1);
 
         return root;
     }
