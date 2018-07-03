e44892913d391330296086c60f6d5b16b8731bf9
Jay Modi <modi.jay6@gmail.com>
2018-07-03 01:09:04 -0500

    with comments

--- src/Recursion/Backtracking/PrintBinary.java
+++ src/Recursion/Backtracking/PrintBinary.java
@@ -3,10 +3,12 @@ package Recursion.Backtracking;
 public class PrintBinary {
 
     public static void main(String[] args) {
-        printBinary(5);
+        printBinary(2);
     }
 
     private static void printBinary(int digits) {
+        // exhaustive search
+        // prefix - is the decisions I have taken
         printBinary(digits, "");
     }
 
@@ -14,6 +16,7 @@ public class PrintBinary {
         if (digits == 0) {
             System.out.println(prefix);
         } else {
+            // explore all options with the decision I have taken & decision is prefix
             printBinary(digits - 1, prefix + "0");
             printBinary(digits - 1, prefix + "1");
         }
