3eec6b7121077a56749e0479e4b46b9bbe906253
Jay Modi <modi.jay6@gmail.com>
2018-07-04 12:28:17 -0500

    one more generic approach with boolean visited array

--- src/Recursion/Backtracking/PermuteNumbersII.java
+++ src/Recursion/Backtracking/PermuteNumbersII.java
@@ -6,30 +6,36 @@ import java.util.List;
 public class PermuteNumbersII {
 
     public static void main(String[] args) {
-        List<List<Integer>> permute = permute(new int[]{1, 2, 3});
+        List<List<Integer>> permute = permute(new int[]{1, 2, 3, 4});
 
         permute.forEach(System.out::println);
     }
 
     public static List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
 
         List<Integer> tempList = new ArrayList<>();
-        permuteHelper(nums, tempList, result);
+        permuteHelper(nums, tempList, result, visited);
 
         return result;
     }
 
-    private static void permuteHelper(int[] nums, List<Integer> tempList, List<List<Integer>> result) {
+    private static void permuteHelper(int[] nums, List<Integer> tempList, List<List<Integer>> result, boolean[] visited) {
         if (tempList.size() == nums.length) {
             result.add(new ArrayList<>(tempList));
         } else {
 
-            for (int num : nums) {
-                if (tempList.contains(num)) continue; // element already exists, skip
-                tempList.add(num);
-                permuteHelper(nums, tempList, result);
-                tempList.remove(tempList.size() - 1);
+            for (int i = 0; i < nums.length; i++) {
+                if (!visited[i]) {
+                    tempList.add(nums[i]);
+                    visited[i] = true;
+
+                    permuteHelper(nums, tempList, result, visited);
+
+                    tempList.remove(tempList.size() - 1);
+                    visited[i] = false;
+                }
             }
         }
     }
