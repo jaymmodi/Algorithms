4dad107b877edb8c9a53d0432613588e67387ca4
Jay Modi <modi.jay6@gmail.com>
2018-07-17 11:07:35 -0500

    catalan number with dynamic programming - leetcode

--- src/Trees/UniqueBinarySearchTrees.java
+++ src/Trees/UniqueBinarySearchTrees.java
@@ -4,22 +4,30 @@ public class UniqueBinarySearchTrees {
 
     // Catalan number
     public static void main(String[] args) {
-        System.out.println(numTrees(3));
+        System.out.println(numTrees(19));
     }
 
     public static int numTrees(int n) {
-        if (n == 0) {
-            return 1;
+        int memory[] = new int[n+1];
+        memory[0] =1;
+        if(n >= 1){
+            memory[1] =1;
         }
-        if (n <= 2) {
-            return n;
+
+        return numTreesHelper(n, memory);
+    }
+
+    private static int numTreesHelper(int n, int[] memory){
+        if(memory[n]!=0){
+            return memory[n];
         }
 
         int sum = 0;
         for (int i = 0; i < n; i++) {
-            sum = sum + numTrees(i) * numTrees(n - 1 - i);
+            sum = sum + numTreesHelper(i, memory) * numTreesHelper(n - 1 - i, memory);
         }
 
+        memory[n] = sum;
         return sum;
     }
 }
