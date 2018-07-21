0dbb18432e7806ac31b811790ea7dd1e569e0848
Jay Modi <modi.jay6@gmail.com>
2018-07-21 02:10:09 -0500

    find duplicate number

---
+++ src/Misc/DuplicateNumber.java
@@ -0,0 +1,17 @@
+package Misc;
+
+public class DuplicateNumber {
+    
+
+    public int findDuplicate(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                return index + 1;
+            }
+            nums[index] = nums[index] * -1;
+        }
+        return -1;
+    }
+}
